1387ba3036fa9fb4ee91a74dd52cd1968040dc85
Max Haslbeck <maxhaslbeck@web.de>
2011-11-18 21:13:19 +0100

    change sampler.adb

--- sampler.adb
+++ sampler.adb
@@ -1,5 +1,6 @@
 with Pendulum_Io_Sim; use Pendulum_Io_Sim;
 with System; use System;
+with Ada.Text_IO;
 
 package body Sampler is
 
@@ -30,7 +31,7 @@ package body Sampler is
    end Barrier_Sampler;
 
    task body Barrier_Sampler is
-      Barrier_Sampler_Period : constant Time_Span := Milliseconds (50);
+      Barrier_Sampler_Period : constant Time_Span := Milliseconds (10);
       Next                   : Time := Clock;
 
       Circ_Buff_Length : constant Integer := 3;
@@ -46,17 +47,24 @@ package body Sampler is
       Full_Round             : Boolean := False;
       T1, T2, Period               : Time_Span;
       Most_Left              : Time;
+
+      SC : Seconds_Count;
+      TS : Time_Span;
    begin
+
       loop
          Curr_Barrier := Get_Barrier;
          if Curr_Barrier /= Prev_Barrier then
             -- flank
+         Ada.Text_IO.Put_Line("1!");
             if Curr_Barrier then
+         Ada.Text_IO.Put_Line("2!");
                -- rising flank
                Circ_Buff(Circ_Buff_Index) := Clock;
                Circ_Buff_Index := (Circ_Buff_Index + 1) mod Circ_Buff_Length;
 
                if Full_Round then
+         Ada.Text_IO.Put_Line("3!");
                   -- now Circ_Buff_Index points at the oldest rising flank
                   T1 := Circ_Buff ((Circ_Buff_Index + 1) mod Circ_Buff_Length)
                     - Circ_Buff (Circ_Buff_Index);
@@ -73,6 +81,9 @@ package body Sampler is
 
                   Period := Circ_Buff((Circ_Buff_Index + 2) mod Circ_Buff_Length) - Circ_Buff(Circ_Buff_Index);
 
+                  Split(Most_Left, SC, TS);
+
+                  Ada.Text_IO.Put_Line("most left: " & SC'Img & " period: " & To_Duration(Period)'Img);
                end if;
 
             else
@@ -80,17 +91,26 @@ package body Sampler is
                   null;
             end if;
          end if;
+         Ada.Text_IO.Put_Line("3!");
 
          if Circ_Buff_Index = 2 then
             Full_Round := True;
          end if;
 
+         if Most_Left + Period > Clock then
+         	Most_Left := Most_Left + Period;
+         end if;
+
          Sampler_Data.Set_Most_Left_Time (Most_Left);
          Sampler_Data.Set_Period (Period);
 
+
+
          Prev_Barrier := Curr_Barrier;
          Next := Next + Barrier_Sampler_Period;
+         Ada.Text_IO.Put_Line("ende!");
          delay until Next;
+         Ada.Text_IO.Put_Line("dannach!");
       end loop;
    end Barrier_Sampler;
 
