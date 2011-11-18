ba072c42e0cbaa7b96b1451530a3a050aa3d8d50
Jonathan Nilsson <lord.jonil@gmail.com>
2011-11-18 23:14:54 +0100

    Framebuffer done

---
+++ framebuffer.adb
@@ -0,0 +1,30 @@
+package body Framebuffer is
+
+   protected Framebuffer_Data is
+      procedure Set_Pixel (X, Y : Integer, Value : Pixel_Type);
+      procedure Get_Pixel (X, Y : Integer) return Pixel_Type;
+
+   private
+
+   end Framebuffer_Data;
+
+   protected body Framebuffer_Data is
+
+   end Framebuffer_Data;
+
+   procedure Init (N : Integer) is
+   begin
+
+   end Init;
+
+   procedure Set_Pixel (X, Y : Integer, Value : Pixel_Type) is
+   begin
+
+   end Set_Pixel;
+
+   function Get_Pixel (X, Y : Integer) return Pixel_Type is
+   begin
+
+   end Get_Pixel;
+
+end Framebuffer;
--- framebuffer.ads
+++ framebuffer.ads
@@ -1,8 +1,16 @@
 package Framebuffer is
    type Pixel_Type is delta 0.0 .. 1.0;
 
-   procedure Init (N : Integer);
-   procedure Set_Pixel (X, Y : Integer, Value : Pixel_Type);
-   function Get_Pixel (X, Y : Integer) return Pixel_Type;
+   protected Framebuffer_Data is
+      procedure Init (Columns : Integer);
+      procedure Set_Pixel (X, Y : Integer, Value : Pixel_Type);
+      function Get_Pixel (X, Y : Integer) return Pixel_Type;
+      function Get_Rows return Integer;
+      function Get_Columns return Integer;
+   private
+      Rows    : constant Integer := 8;JoNil
+      Columns : Integer := 0;
+      Buffer  : array (0 .. 7, Positive range <>) of Element_Type;
+   end Framebuffer_Data;
 
 end Framebuffer;
