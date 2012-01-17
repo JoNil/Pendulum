4852ff4c3679380895ab0a2069b0925d3d60ed4d
Jonathan Nilsson <lord.jonil@gmail.com>
2012-01-17 19:48:09 +0100

    sine table

--- tools/lookup_table_generator.py
+++ tools/lookup_table_generator.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
-from math import pi, cos, acos
+from math import pi, sin, cos, acos
 
 res = "("
 
-operator = acos;
-table_size = 64
+def v(x):
+    if x <= 0.5:
+        return 1.5-2*x
+    else:
+        return 2*x-0.5
+
+def trig(x):
+    return -sin(x*pi)*0.5+1.35
+
+operator = trig;
+table_size = 72
 domain = 1
 
 for i in range(table_size):
